2aa24684de8c9fc635af29a32f8b1ad5b2bae790
codedm24 <mithun.adroit@gmail.com>
2024-11-29 17:11:59 +0530

    MAUI XAML Basics
    MAUI XAML basics

---
+++ MauiApp-XAML/MauiApp-XAML/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp_XAML
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiApp-XAML/MauiApp-XAML/AppConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp_XAML
+{
+    internal class AppConstants
+    {
+        public static readonly Color CustomBackgroundColor = Colors.Aqua;
+        public static readonly Color CustomForegroundColor = Colors.Brown;
+    }
+}
